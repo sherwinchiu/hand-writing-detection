d667e32ed80b88f33d64be3faccb38abf54cf062
Sherwin Chiu <sherwin.chiu89@gmail.com>
2021-02-26 17:21:17 -0500

    added imports

---
+++ main.py
@@ -0,0 +1,18 @@
+# Imports
+import tensorflow as tf
+from tensorflow import keras
+from keras.models import Sequential
+from keras.layers import Dense, Flatten, Conv2D, MaxPooling2D, Dropout
+from tensorflow.keras import layers
+from keras.utils import to_categorical
+import numpy as np
+import matplotlib.pyplot as plt 
+from keras.datasets import cifar10
+
+
+# Style of pyplot
+plt.style.use('fivethirtyeight')
+
+# Loading data
+
+(x_train, y_train), (x_test, y_test) = cifar10.load_data()
