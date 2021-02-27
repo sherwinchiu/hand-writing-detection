52f1469c0cf8f2cbea7d39250571ecc0484b1154
v341196137 <38384400+v341196137@users.noreply.github.com>
2021-02-26 23:20:51 -0500

    committing a working version because *REASONS*

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 # Imports
+import cv2
+from identify import classification
 import tensorflow as tf
-from tensorflow import keras, models
+from tensorflow.keras import models
 from keras.models import Sequential
 from keras.layers import Dense, Flatten, Conv2D, MaxPooling2D, Dropout
 from tensorflow.keras import layers
@@ -8,6 +10,7 @@ from keras.utils import to_categorical
 import numpy as np
 import matplotlib.pyplot as plt 
 from keras.datasets import cifar10
+from skimage.transform import resize
 
 
 # Style of pyplot
@@ -15,6 +18,11 @@ plt.style.use('fivethirtyeight')
 
 # Loading data
 
-(x_train, y_train), (x_test, y_test) = cifar10.load_data()
+# (x_train, y_train), (x_test, y_test) = cifar10.load_data()
 model = models.load_model("handwriting.model")
-# TODO: predict letter
+path = input("give path to image")
+img = cv2.imread(path)
+img = resize(img, (32, 32, 3))
+prediction = model.predict(np.array([img])/255)
+index = np.argmax(prediction)
+print(classification[index])
